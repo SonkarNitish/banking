8e93107187520fbfe5572fe040e43adce28ca034
Schaaz <schaaz3allme@gmail.com>
2025-05-29 14:32:10 +0530

    New commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Idk'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext = "Acess and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
